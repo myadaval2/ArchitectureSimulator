aca4fce9d7b34e8d4d4d9969279fdba70d3c08c8
jagath-jaikumar <jjaikumar@wpi.edu>
2019-02-13 15:13:46 -0500

    started java project

---
+++ architecturesim/src/src/Driver.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author jjaikumar
+ */
+public class Driver {
+    public static void main(String [ ] args) {   
+       System.out.println("Architecture Simulator 535");
+    }
+}

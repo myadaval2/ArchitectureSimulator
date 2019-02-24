c49fbd9feb0849f117d83da2c074b30baf45063b
jagath-jaikumar <jjaikumar@wpi.edu>
2019-02-24 17:44:09 -0500

    restructured caches

--- architecturesim/src/src/Cache.java
+++ architecturesim/src/src/Cache.java
@@ -12,108 +12,21 @@ package src;
 public class Cache {
     // public int lineLength;
     private int sizeOfMemory;
-    private Cache lowerLevelMemory;
     private int waitCycles;
     private int counter;
-    private boolean fetchingMemory;
-    private boolean isDRAM;
     private char[] mem_array;
     private int[] tag_array;
-    private int levelsFromMain;
-    
-    // how do we want memory to look? vector of arrays? 2D array with row = 4 words, and 128 rows for DRAM?
-
     
     public Cache(int sizeOfMemory, Cache lowerLevelMemory, int waitCycles) {
         this.sizeOfMemory = sizeOfMemory;
-        this.lowerLevelMemory = lowerLevelMemory;
         this.waitCycles = waitCycles;
         this.counter = waitCycles;
-        this.isDRAM = false;
         
         this.mem_array = new char[sizeOfMemory];
         this.tag_array = new int[sizeOfMemory];
-        
-        if (lowerLevelMemory == null){
-            this.isDRAM = true;
-        }
-        this.levelsFromMain = levelsFromMain();
     }
 
-
-    public void writeAddressInMemory(char data, int address) throws NoSuchMemoryLocationException {
-        boolean exists = false;
-        try {
-            exists = addressInCurrMemoryLevel(address);
-        } catch (NoSuchMemoryLocationException e) {
-            throw e;
-        }
-        
-        if (!exists) {
-            this.lowerLevelMemory.writeAddressInMemory(data, address);
-        } 
-        while (this.counter != 0){
-            delayCounter(); 
-        } 
  
-        if (isDRAM) {
-            mem_array[address] = data; 
-        }
-        else {
-            char MASK_INDEX = 0;
-            if (this.levelsFromMain == 2) { // L1 Cache
-                MASK_INDEX = 16383; // BINARY 0011 1111 1111 1111
-                
-            } else { // levelsFromMain = 1, L2 Cache
-                MASK_INDEX = 32767; // BINARY 0111 1111 1111 1111
-            }
-            int index_bit = address & MASK_INDEX;
-            mem_array[index_bit] = data;
-        }
-    }
-    
-    
-    private void writeToL1(char data, int address){
-        char MASK_INDEX = 16383;
-        
-        int index = address & MASK_INDEX;
-        
-        mem_array[index] = data;
-    }
-    public int getAddressInMemory(int address) throws NoSuchMemoryLocationException{
-        
-        boolean exists = false;
-        try {
-            exists = addressInCurrMemoryLevel(address);
-        } catch (NoSuchMemoryLocationException e) {
-            throw e;
-        }
-        
-        if (!exists) {
-            return this.lowerLevelMemory.getAddressInMemory(address);
-        } 
-        while (this.counter != 0){
-            delayCounter(); 
-        } 
- 
-        if (isDRAM){
-           char temp = mem_array[address];
-           writeToL1(temp, address);
-           return temp;  
-        }  
-        else {
-            char MASK_INDEX = 0;
-            if (this.levelsFromMain == 2) { // L1 Cache
-                MASK_INDEX = 16383; // BINARY 0011 1111 1111 1111
-                
-            } else { // levelsFromMain = 1, L2 Cache
-                MASK_INDEX = 32767; // BINARY 0111 1111 1111 1111
-            }
-            int index_bit = address & MASK_INDEX;
-                
-            return mem_array[index_bit];
-        }
-    }
     
     private Boolean addressInCurrMemoryLevel(int address) throws NoSuchMemoryLocationException{
         // if address is in array, return true, else return false
@@ -141,39 +54,15 @@ public class Cache {
                 throw new NoSuchMemoryLocationException(address);
         }
     }
-        
-    private int levelsFromMain(){
-        if (this.isDRAM){
-            return 0;
-        }
-        int temp = 0;
-        Cache pointer = this;
-        while (pointer.lowerLevelMemory != null){
-            pointer = pointer.lowerLevelMemory;
-            temp++;
-        }
-        return temp;
-    }
     
     private void delayCounter() {
         this.counter = this.counter--;
     }
     
     public int getSizeOfMemory()        {   return sizeOfMemory;        }
-    public Cache getLowerLevelMemory() {   return lowerLevelMemory;    }
     public int getWaitCycles()          {   return waitCycles;          }
     public int getCounter()             {   return counter;             }
-    public Boolean getFetchingMemory()  {   return fetchingMemory;      }
     public char[] getMemArray()         {   return mem_array;           }
 }
 
-class NoSuchMemoryLocationException extends Exception {
-    int address;
-    public NoSuchMemoryLocationException(int address){
-        this.address = address;
-        System.out.println("No such memory location exists: "+ address);
-    }
-    public NoSuchMemoryLocationException(){
-        System.out.println("No such memory location exists");
-    }
-}
+
--- architecturesim/src/src/Memory.java
+++ architecturesim/src/src/Memory.java
@@ -13,21 +13,87 @@ public class Memory {
     public Cache DRAM; 
     public Cache L1Cache;
     public Cache L2Cache;
-    public Cache currMemoryLevel;
-    public Cache topLevelMemory;
     
     
     public Memory() {
         DRAM = new Cache(65536, null, 100); // 2^16 Bytes
         L2Cache = new Cache(32768,DRAM,30); // 2^15 Bytes
         L1Cache = new Cache(16384,L2Cache,4); // 2&14 Bytes
-        currMemoryLevel = L1Cache;
-        topLevelMemory = L1Cache;
         
-        // read data in L1Cache
-        // write data in L1Cache
-        // read data in DRAM
-        // write data in DRAM
+    }
+       public int getAddressInMemory(int address) throws NoSuchMemoryLocationException{
+        
+        boolean exists = false;
+        try {
+            exists = addressInCurrMemoryLevel(address);
+        } catch (NoSuchMemoryLocationException e) {
+            throw e;
+        }
+        
+        if (!exists) {
+            return this.lowerLevelMemory.getAddressInMemory(address);
+        } 
+        while (this.counter != 0){
+            delayCounter(); 
+        } 
+ 
+        if (isDRAM){
+           char temp = mem_array[address];
+           writeToL1(temp, address);
+           return temp;  
+        }  
+        else {
+            char MASK_INDEX = 0;
+            if (this.levelsFromMain == 2) { // L1 Cache
+                MASK_INDEX = 16383; // BINARY 0011 1111 1111 1111
+                
+            } else { // levelsFromMain = 1, L2 Cache
+                MASK_INDEX = 32767; // BINARY 0111 1111 1111 1111
+            }
+            int index_bit = address & MASK_INDEX;
+                
+            return mem_array[index_bit];
+        }
+    }
+    public void writeAddressInMemory(char data, int address) throws NoSuchMemoryLocationException {
+        boolean exists = false;
+        try {
+            exists = addressInCurrMemoryLevel(address);
+        } catch (NoSuchMemoryLocationException e) {
+            throw e;
+        }
+        
+        if (!exists) {
+            this.lowerLevelMemory.writeAddressInMemory(data, address);
+        } 
+        while (this.counter != 0){
+            delayCounter(); 
+        } 
+ 
+        if (isDRAM) {
+            mem_array[address] = data; 
+        }
+        else {
+            char MASK_INDEX = 0;
+            if (this.levelsFromMain == 2) { // L1 Cache
+                MASK_INDEX = 16383; // BINARY 0011 1111 1111 1111
+                
+            } else { // levelsFromMain = 1, L2 Cache
+                MASK_INDEX = 32767; // BINARY 0111 1111 1111 1111
+            }
+            int index_bit = address & MASK_INDEX;
+            mem_array[index_bit] = data;
+        }
     }
     
+}
+class NoSuchMemoryLocationException extends Exception {
+    int address;
+    public NoSuchMemoryLocationException(int address){
+        this.address = address;
+        System.out.println("No such memory location exists: "+ address);
+    }
+    public NoSuchMemoryLocationException(){
+        System.out.println("No such memory location exists");
+    }
 }

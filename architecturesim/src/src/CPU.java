57185de630bbdadc92ce0a3250816f1c41a0bd82
Meghana Yadavalli <yadavalli.meghana@gmail.com>
2019-02-26 12:55:22 -0500

    enhanced the GUI a little, simple read and write test cases added, caches and DRAM seem to work

--- architecturesim/src/src/CPU.java
+++ architecturesim/src/src/CPU.java
@@ -23,10 +23,25 @@ public class CPU {
     public int getClock()       {       return memory.getMemoryCycleCount();       }
     
     public void testCase1() {
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
         memory.writeAddressInMemory((char) 0xffff, 0);
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
         memory.writeAddressInMemory((char) 0xffff, 0x1ffff);
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
+        memory.writeAddressInMemory((char) 0xffff, 0x1fffe);
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
+        memory.writeAddressInMemory((char) 0xffff, 0x1fffd);
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
+        memory.writeAddressInMemory((char) 0xffff, 0x1fffc);
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
+        memory.writeAddressInMemory((char) 0xaaaa, 0x7fff);
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
         memory.writeAddressInMemory((char) 0xaaaa, 0);
-        System.out.println(memory.getMemoryCycleCount());
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
+        System.out.println(memory.readAddressInMemory(0x7fff));
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
+        System.out.println(memory.readAddressInMemory(0x1ffff));
+        System.out.println("Clock cycle count: " + memory.getMemoryCycleCount());
     }
     
     // During MEM stage CPU.clock += memory.getmemoryCycleCount();
